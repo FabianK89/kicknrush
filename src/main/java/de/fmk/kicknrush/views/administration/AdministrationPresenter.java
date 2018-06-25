3778ff1dedcca5baea73979b5f293d21e95300d5
FabianK89 <fabian.kiesl@gmail.com>
2018-06-26 00:00:31 +0200

    created Administration view

---
+++ src/main/java/de/fmk/kicknrush/views/administration/AdministrationPresenter.java
@@ -0,0 +1,13 @@
+package de.fmk.kicknrush.views.administration;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdministrationPresenter implements Initializable {
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+}
---
+++ src/main/java/de/fmk/kicknrush/views/administration/AdministrationView.java
@@ -0,0 +1,6 @@
+package de.fmk.kicknrush.views.administration;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+public class AdministrationView extends FXMLView {
+}
