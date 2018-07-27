633e3111adbdb197355faf153852dca7516e0daa
FabianK89 <fabian.kiesl@gmail.com>
2018-07-27 16:46:29 +0200

    team administration

--- src/main/java/de/fmk/kicknrush/models/administration/teamadministration/TeamAdministrationModel.java
+++ src/main/java/de/fmk/kicknrush/models/administration/teamadministration/TeamAdministrationModel.java
@@ -1,22 +1,72 @@
 package de.fmk.kicknrush.models.administration.teamadministration;
 
+import de.fmk.kicknrush.db.DatabaseHandler;
 import de.fmk.kicknrush.helper.ThreadHelper;
 import de.fmk.kicknrush.helper.UpdateHelper;
 import de.fmk.kicknrush.models.AbstractStatusModel;
 import de.fmk.kicknrush.models.Status;
+import de.fmk.kicknrush.models.pojo.Team;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.image.Image;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 
 /**
  * @author FabianK
  */
 public class TeamAdministrationModel extends AbstractStatusModel {
+	private static final Logger LOGGER = LoggerFactory.getLogger(TeamAdministrationModel.class);
+
+	private final ObservableList<Team> teams;
+
 	@Inject
-	private UpdateHelper updateHelper;
+	private DatabaseHandler dbHandler;
 	@Inject
-	private ThreadHelper threadHelper;
+	private UpdateHelper    updateHelper;
+	@Inject
+	private ThreadHelper    threadHelper;
+
+
+	public TeamAdministrationModel() {
+		super();
+
+		teams = FXCollections.observableArrayList();
+	}
+
+
+	public void saveChanges(final Team team) {
+		final Thread thread;
+
+		if (team == null)
+			throw new IllegalArgumentException("The team object must not be null.");
+
+		statusProperty.set(Status.RUNNING);
+
+		thread = new Thread(() -> {
+			if (dbHandler.insertOrUpdateTeam(team))
+				Platform.runLater(() -> statusProperty().set(Status.SUCCESS));
+			else
+				Platform.runLater(() -> statusProperty().set(Status.FAILED));
+
+			threadHelper.removeThread(Thread.currentThread().getName());
+		}, "saveTeam".concat(Integer.toString(team.getTeamId())));
+
+		threadHelper.addThread(thread);
+
+		thread.start();
+	}
 
 
 	@PostConstruct
@@ -31,12 +81,57 @@ public class TeamAdministrationModel extends AbstractStatusModel {
 		statusProperty.set(Status.RUNNING);
 
 		thread = new Thread(() -> {
+			final List<Team> dbTeams;
+
 			updateHelper.checkForUpdates();
-			statusProperty().set(Status.SUCCESS);
-		});
+
+			dbTeams = dbHandler.selectAllTeams();
+
+			Platform.runLater(() -> {
+				teams.clear();
+				teams.addAll(dbTeams);
+				statusProperty().set(Status.SUCCESS);
+			});
+
+			threadHelper.removeThread(Thread.currentThread().getName());
+		}, "updateTeams");
 
 		threadHelper.addThread(thread);
 
 		thread.start();
+	}
+
+
+	public Image getImage(final String imagePath) {
+		final Path path;
+
+		if (imagePath == null)
+			return null;
+
+		if (isWebUrl(imagePath))
+			return new Image(imagePath);
+
+		path = Paths.get(imagePath);
+
+		if (imagePath.toLowerCase().endsWith(".png") && Files.isRegularFile(path)) {
+			try (InputStream is = Files.newInputStream(path)) {
+				return new Image(is);
+			}
+			catch (IOException ioex) {
+				LOGGER.error("Could not load the image file '{}'.", imagePath, ioex);
+			}
+		}
+
+		return null;
+	}
+
+
+	public boolean isWebUrl(final String url) {
+		return url != null && url.matches("http[s]?://.+");
+	}
+
+
+	public ObservableList<Team> getTeams() {
+		return teams;
 	}
 }
--- src/main/java/de/fmk/kicknrush/views/administration/teamadministration/TeamAdministrationPresenter.java
+++ src/main/java/de/fmk/kicknrush/views/administration/teamadministration/TeamAdministrationPresenter.java
@@ -3,11 +3,22 @@ package de.fmk.kicknrush.views.administration.teamadministration;
 import de.fmk.kicknrush.helper.UTF8Resources;
 import de.fmk.kicknrush.models.Status;
 import de.fmk.kicknrush.models.administration.teamadministration.TeamAdministrationModel;
+import de.fmk.kicknrush.models.pojo.Team;
+import de.fmk.kicknrush.views.AppImage;
 import de.fmk.kicknrush.views.INotificationPresenter;
 import de.fmk.kicknrush.views.Notification;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
 
 import javax.inject.Inject;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,10 +27,30 @@ import java.util.ResourceBundle;
  * @author FabianK
  */
 public class TeamAdministrationPresenter implements INotificationPresenter, Initializable {
+	@FXML
+	private Button         cancelButton;
+	@FXML
+	private Button         saveButton;
+	@FXML
+	private GridPane       detailPane;
+	@FXML
+	private ImageView      icon;
+	@FXML
+	private ImageView      smallIcon;
+	@FXML
+	private ListView<Team> teamList;
+	@FXML
+	private TextField      nameInput;
+	@FXML
+	private TextField      iconPathInput;
+	@FXML
+	private TextField      smallIconPathInput;
+
 	@Inject
 	private TeamAdministrationModel model;
 
 	private Notification  notificationPane;
+	private Team          selectedTeam;
 	private UTF8Resources resources;
 
 
@@ -33,14 +64,58 @@ public class TeamAdministrationPresenter implements INotificationPresenter, Init
 	public void initialize(URL location, ResourceBundle resources) {
 		this.resources = new UTF8Resources(resources);
 
+		nameInput.textProperty().addListener(observable -> disableSaveButton());
+
+		iconPathInput.textProperty().addListener((pathInputObservable, oldPath, newPath) -> {
+			icon.setImage(model.getImage(newPath));
+			disableSaveButton();
+		});
+		smallIconPathInput.textProperty().addListener((pathInputObservable, oldPath, newPath) -> {
+			smallIcon.setImage(model.getImage(newPath));
+			disableSaveButton();
+		});
+
+		teamList.setItems(model.getTeams());
+		teamList.setCellFactory(list -> new TeamCell());
+		teamList.getSelectionModel().selectedItemProperty().addListener((observable, last, current) -> {
+			selectedTeam = current;
+
+			detailPane.setVisible(current != null);
+
+			if (current == null)
+				return;
+
+			nameInput.setText(current.getTeamName());
+
+			smallIconPathInput.setText(current.getTeamIconUrlSmall());
+			iconPathInput.setText(current.getTeamIconUrl());
+			saveButton.setDisable(true);
+		});
+
 		model.statusProperty().addListener((observable, oldStatus, newStatus) -> {
 			if (Status.RUNNING == newStatus) {
 				notificationPane.showLoading(this.resources.get("msg.loading"));
 			}
 			else if (Status.SUCCESS == newStatus) {
-				notificationPane.hide();
+				notificationPane.showSuccess("Test", AppImage.SUCCESS);
 			}
 		});
+
+		detailPane.setVisible(false);
+	}
+
+
+	private void disableSaveButton() {
+		final boolean changedIcon;
+		final boolean changedName;
+		final boolean changedSmallIcon;
+
+		changedName      = !selectedTeam.getTeamName().equals(nameInput.getText());
+		changedIcon      = !selectedTeam.getTeamIconUrl().equals(iconPathInput.getText());
+		changedSmallIcon = !selectedTeam.getTeamIconUrlSmall().equals(smallIconPathInput.getText());
+
+		saveButton.setDisable(nameInput.getText() == null || icon.getImage() == null || smallIcon.getImage() == null ||
+		                      !changedName && !changedIcon && !changedSmallIcon);
 	}
 
 
@@ -53,5 +128,68 @@ public class TeamAdministrationPresenter implements INotificationPresenter, Init
 	@Override
 	public void setNotificationPane(Notification pane) {
 		notificationPane = pane;
+	}
+
+
+	@FXML
+	private void onCancel() {
+		teamList.getSelectionModel().clearSelection();
+		teamList.refresh();
+	}
+
+
+	@FXML
+	private void onChooseIcon() {
+		iconPathInput.setText(chooseIcon(iconPathInput.getText()));
+	}
+
+
+	@FXML
+	private void onChooseSmallIcon() {
+		smallIconPathInput.setText(chooseIcon(smallIconPathInput.getText()));
+	}
+
+
+	@FXML
+	private void onSave() {
+		selectedTeam.setTeamName(nameInput.getText());
+		selectedTeam.setTeamIconUrl(iconPathInput.getText());
+		selectedTeam.setTeamIconUrlSmall(smallIconPathInput.getText());
+
+		model.saveChanges(selectedTeam);
+
+		teamList.refresh();
+	}
+
+
+	private String chooseIcon(final String path) {
+		final File        imageFile;
+		final FileChooser fc;
+
+		fc = new FileChooser();
+		fc.getExtensionFilters().add(new FileChooser.ExtensionFilter(resources.get("file.images"), "*.png"));
+
+		if (!model.isWebUrl(path))
+			fc.setInitialFileName(path);
+
+		imageFile = fc.showOpenDialog(teamList.getScene().getWindow());
+
+		if (imageFile != null && imageFile.exists())
+			return imageFile.getAbsolutePath();
+
+		return path;
+	}
+
+
+	private class TeamCell extends ListCell<Team> {
+		@Override
+		protected void updateItem(Team team, boolean empty) {
+			super.updateItem(team, empty);
+
+			if (team == null || empty)
+				setText(null);
+			else
+				setText(team.getTeamName());
+		}
 	}
 }
